fc76c9829c51733a38d5a49dc0859caf6a758253
Marvin Forster <marvinforster@yahoo.de>
2025-12-17 19:00:42 +0100

    updated affiliate links for amazon and added more languages

--- app/Home.js
+++ app/Home.js
@@ -25,9 +25,10 @@ export default function Home({ initialLang = "en" }) {
 
     if (lang.startsWith("de")) return "amazon.de";
     if (lang.startsWith("en-GB")) return "amazon.co.uk";
-    if (lang.startsWith("fr")) return "amazon.fr";  // TODO
-    if (lang.startsWith("it")) return "amazon.it";  // TODO
-    if (lang.startsWith("es")) return "amazon.es";  // TODO
+    if (lang.startsWith("us")) return "amazon.com";
+    if (lang.startsWith("fr")) return "amazon.fr";
+    if (lang.startsWith("it")) return "amazon.it";
+    if (lang.startsWith("es")) return "amazon.es";
 
     return "amazon.com"; // fallback
   }
@@ -38,11 +39,11 @@ export default function Home({ initialLang = "en" }) {
 
     const tags = {
       "amazon.de": "surprisehub01-21",
-      "amazon.com": "deinUStag-20", //TODO
-      "amazon.co.uk": "deinUKtag-21", //TODO
-      "amazon.fr": "deinFRtag-21", //TODO
-      "amazon.it": "deinITtag-21", //TODO
-      "amazon.es": "deinEStag-21", //TODO
+      "amazon.com": "surprisehub0c-20",
+      "amazon.co.uk": "surprisehub0d-21",
+      "amazon.fr": "surprisehub09-21",
+      "amazon.it": "surprisehub0f-21",
+      "amazon.es": "surprisehub04-21",
     };
 
     const tag = tags[domain] || tags["amazon.com"];
